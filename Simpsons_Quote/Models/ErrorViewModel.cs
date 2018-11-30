d959c8bfa318503d499a6e7bc79a0f096bed8332
michifehr <mifehr@bluewin.ch>
2018-11-30 08:59:12 +0100

    first commit

---
+++ Simpsons_Quote/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Simpsons_Quote.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Simpsons_Quote/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function myFunction(){
+    fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myArray) {
+          console.log(myArray[0]);
+          var obj = myArray[0];
+          document.getElementById("generated_image").src = obj.image;
+          document.getElementById("name").innerHTML = obj.character;
+          document.getElementById("quote").innerHTML = obj.quote;
+      });
+}
+
+
+
+
+
+
+
+
